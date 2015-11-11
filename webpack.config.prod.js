ee960ff96d475c6d816e1572b2de4e7ea09796aa
potsrevennil <b02901144@ntu.edu.tw>
2015-11-11 09:39:33 +0800

    modify the path

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,28 +2,37 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: [
-    'webpack-hot-middleware/client',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/hw-reference/7-class-demo-todo/dist/'
+    publicPath: '/dist/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
   ],
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
-    }, {
-      test: /\.css$/,
-      loaders: ['style', 'css']
     }]
   }
 };
+
+    
+
+
